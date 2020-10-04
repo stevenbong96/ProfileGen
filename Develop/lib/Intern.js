4b759af8c0b347475be209c24474240f6e7031d4
Steven Bong <stevenseal99@yahoo.com>
2020-10-03 21:56:12 -0700

    Run the npm run test. Scan through the failed tests. Start working on first fail test by trying few things on intern.js file

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,5 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
 
+// Create constructor function
+// function school(){
+//     getRole(){
+//         return "Intern"
+//     }
+//     getSchool(){
+//         return this.school;
+//     }
+// }
 
 // Exports this file
 // module.exports = Intern;
